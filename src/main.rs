70bc0765bbe8aff5665cdcf0a6994b31b244cab7
Lilly Emerson <lillia.e42@gmail.com>
2025-05-09 17:56:59 -0600

    simple text input of meal options
    Signed-off-by: Lilly Emerson <lillia.e42@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Collaborative meal planner");
+
+    let mut meals: Vec<String> = Vec::new();
+
+    loop {
+        println!("Please input a meal option: ");
+
+        let mut entry = String::new();
+
+        io::stdin()
+            .read_line(&mut entry)
+            .expect("Unable to read meal option");
+
+        let meal = entry.trim().to_owned();
+        meals.push(meal.clone());
+
+        println!("\nYou entered: '{meal}'\n");
+
+        println!("So far you have entered the following meals:");
+        for item in &meals {
+            println!("\t{item}");
+        }
+        println!()
+    }
 }
