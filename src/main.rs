a971a3c38b0a6d45468d104548d9b997e78ba6ca
Lilly Emerson <lillia.e42@gmail.com>
2025-05-16 14:11:09 -0600

    track times someone enters meal name as votes
    Signed-off-by: Lilly Emerson <lillia.e42@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
 use std::io;
 
 fn main() {
     println!("Collaborative meal planner");
 
-    let mut meals: Vec<String> = Vec::new();
+    let mut tracker = MealOptionTracker {
+        options: HashMap::new(),
+    };
 
     loop {
         println!("Please input a meal option: ");
@@ -14,15 +17,32 @@ fn main() {
             .read_line(&mut entry)
             .expect("Unable to read meal option");
 
-        let meal = entry.trim().to_owned();
+        let meal_name = entry.trim().to_owned();
 
-        println!("\nYou entered: '{meal}'\n");
-        meals.push(meal);
+        println!("\nYou entered: '{meal_name}'\n");
+
+        tracker.add(&meal_name);
 
         println!("So far you have entered the following meals:");
-        for item in &meals {
-            println!("\t{item}");
+        for (key, option) in &tracker.options {
+            let votes = option.votes;
+            println!("\t{key} ({votes} votes)");
         }
         println!()
+    }
+}
+
+struct MealOption {
+    name: String,
+    votes: i64
+}
+
+struct MealOptionTracker {
+    options: HashMap<String, MealOption>
+}
+
+impl MealOptionTracker {
+    fn add(&mut self, name: &String) {
+        self.options.entry(name.clone()).and_modify(|e| e.votes += 1).or_insert(MealOption {name: name.clone(), votes: 0});
     }
 }
